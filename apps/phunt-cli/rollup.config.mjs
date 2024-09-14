f54b9725e15b2d9d341a0dacb79da7bd87220712
Vladimir Jelovac <3627139+jelovac@users.noreply.github.com>
2024-09-14 16:37:43 +0200

    Preparing CLI app for release by injecting version details

--- apps/phunt-cli/rollup.config.mjs
+++ apps/phunt-cli/rollup.config.mjs
@@ -1,12 +1,17 @@
-import baseOptions from "@112dev/phunt-rollup-config/base-options.mjs"
-import {assembleRollupConfig} from "@112dev/phunt-rollup-config/functions.mjs"
-import {cleanOnBuildStart} from "@112dev/phunt-rollup-config/plugins/clean-on-build-start.mjs";
-import {nodeResolve} from "@rollup/plugin-node-resolve";
+import baseOptions from "@112dev/phunt-rollup-config/base-options.mjs";
+import { assembleRollupConfig } from "@112dev/phunt-rollup-config/functions.mjs";
+import { cleanOnBuildStart } from "@112dev/phunt-rollup-config/plugins/clean-on-build-start.mjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import copy from "rollup-plugin-copy";
+import replace from "@rollup/plugin-replace";
+import { readFileSync } from "fs";
+import { env } from "process";
+
+const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 /**
  * @type {import("rollup").RollupOptions}
@@ -18,11 +23,20 @@ const rollupOptions = {
     copy({
       targets: [
         {
-          src: 'node_modules/@112dev/phunt-core/node_modules/@112dev/phunt-db/node_modules/better-sqlite3/**/*',
-          dest: 'dist/node_modules/better-sqlite3'
+          src: "node_modules/@112dev/phunt-core/node_modules/@112dev/phunt-db/node_modules/better-sqlite3/**/*",
+          dest: "dist/node_modules/better-sqlite3",
         },
       ],
-      verbose: true
+      verbose: true,
+    }),
+    replace({
+      __PHUNT_CLI_VERSION__: JSON.stringify(pkg.version),
+      __PHUNT_CLI_BUILD_ID__: () => {
+        return env["BUILD_ID"] ?? "unknown";
+      },
+      __PHUNT_CLI_BUILD_DATE__: () => {
+        return JSON.stringify(new Date());
+      },
     }),
     nodeResolve({
       preferBuiltins: false,
@@ -36,7 +50,7 @@ const rollupOptions = {
     }),
     typescript({
       tsconfig: "tsconfig.lib.json",
-      moduleResolution: 'bundler'
+      moduleResolution: "bundler",
     }),
     terser({
       sourceMap: true,
--- apps/phunt-cli/src/index.ts
+++ apps/phunt-cli/src/index.ts
@@ -6,12 +6,12 @@ import buildIndexCommand from "./commands/index.command";
 
 const app = new Command();
 
-if (process.env["npm_package_version"] !== undefined) {
-  app.version(process.env["npm_package_version"]);
-}
+app.version(`Version: __PHUNT_CLI_VERSION__
+Build Id: __PHUNT_CLI_BUILD_ID__
+Build Date: __PHUNT_CLI_BUILD_DATE__`);
 
 app.description(`
-Phunt is a command line tool which helps you organize digital photos. You provide the source path(s) from where to look for
+phunt-cli is a command line tool which helps you organize digital photos. You provide the source path(s) from where to look for
 photos and the destination directory. In addition, you can customize the logic how the photos are organized at the
 destination.
 `);
