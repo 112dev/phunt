e6d1719f1b67a15f372f76a62bd7aa57f423636d
Vladimir Jelovac <3627139+jelovac@users.noreply.github.com>
2024-09-26 20:32:50 +0200

    Excluding node_modules from being injected in the generated bundle by marking them as external, and doing so by reading "dependencies" section from package.json. Good thing about this is that there is no longer need to deal with better-sqlite3 node bindings. As all dependencies are external. So in the final build we would just copy the whole node_modules folder together with dist when making a distribution artifact.
    In addition:
    
    Moved base options from phunt-rollup-config to phunt-cli config as the merging of object properties was shallow so not all keys got integrated from both base file and the main. As I did not want to waste any more time on this decided that for now it is good enough to just move the settings.
    
    Removed rollup terser plugin as I don't see currently the need to minify the generated bundle and it added addional overhead when debugging.
    
    Refs #6

--- apps/phunt-cli/rollup.config.mjs
+++ apps/phunt-cli/rollup.config.mjs
@@ -1,12 +1,9 @@
-import baseOptions from "@112dev/phunt-rollup-config/base-options.mjs";
 import { assembleRollupConfig } from "@112dev/phunt-rollup-config/functions.mjs";
 import { cleanOnBuildStart } from "@112dev/phunt-rollup-config/plugins/clean-on-build-start.mjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import terser from "@rollup/plugin-terser";
-import copy from "rollup-plugin-copy";
 import replace from "@rollup/plugin-replace";
 import { readFileSync } from "fs";
 import { env } from "process";
@@ -17,21 +14,27 @@ const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
  * @type {import("rollup").RollupOptions}
  */
 const rollupOptions = {
-  ...baseOptions,
+  input: "src/index.ts",
   output: {
     file: `dist/phunt-cli.js`,
+    format: "cjs",
+    generatedCode: "es2015",
+    sourcemap: true,
+  },
+  strictDeprecations: true,
+  treeshake: {
+    moduleSideEffects: false,
+    propertyReadSideEffects: false,
+    tryCatchDeoptimization: false,
+  },
+  onwarn: (warning, defaultHandler) => {
+    if (warning.code === "CIRCULAR_DEPENDENCY") {
+      return; // Suppress circular dependency warnings.
+    }
+    defaultHandler(warning);
   },
   plugins: [
     cleanOnBuildStart("dist"),
-    copy({
-      targets: [
-        {
-          src: "node_modules/@112dev/phunt-core/node_modules/@112dev/phunt-db/node_modules/better-sqlite3/**/*",
-          dest: "dist/node_modules/better-sqlite3",
-        },
-      ],
-      verbose: true,
-    }),
     replace({
       values: {
         __PHUNT_CLI_NAME__: () => {
@@ -54,19 +57,13 @@ const rollupOptions = {
     json(),
     commonjs({
       ignoreTryCatch: false,
-      dynamicRequireTargets: [
-        "node_modules/better-sqlite3/build/Release/better_sqlite3.node",
-      ],
     }),
     typescript({
       tsconfig: "tsconfig.lib.json",
       moduleResolution: "bundler",
     }),
-    terser({
-      sourceMap: true,
-    }),
   ],
-  external: ["better-sqlite3"],
+  external: Object.keys(pkg.dependencies || {}), // https://github.com/rollup/rollup-plugin-node-resolve/issues/77
 };
 
 export default assembleRollupConfig(rollupOptions);
