727d1617f4a9054f2df9de6cb54d382de45ac9ad
Vladimir Jelovac <3627139+jelovac@users.noreply.github.com>
2024-11-06 16:07:26 +0100

    Migrated phunt-db build from simple tsc to rollup

---
+++ packages/phunt-db/rollup.config.mjs
@@ -0,0 +1,44 @@
+import { assembleRollupConfig } from "@112dev/phunt-rollup-config/functions.mjs";
+import { cleanOnBuildStart } from "@112dev/phunt-rollup-config/plugins/clean-on-build-start.mjs";
+import typescript from "@rollup/plugin-typescript";
+import { readFileSync } from "node:fs";
+
+const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
+
+/**
+ * @type {import("rollup").RollupOptions}
+ */
+const rollupOptions = {
+  input: "src/index.ts",
+  output: [
+    {
+      dir: "dist",
+      format: "es",
+      preserveModules: true,
+      entryFileNames: "[name].mjs",
+      sourcemap: true,
+    },
+    {
+      dir: "dist",
+      format: "cjs",
+      preserveModules: true,
+      entryFileNames: "[name].cjs",
+      sourcemap: true,
+    },
+  ],
+  strictDeprecations: true,
+  treeshake: {
+    moduleSideEffects: false,
+    propertyReadSideEffects: false,
+    tryCatchDeoptimization: false,
+  },
+  plugins: [
+    cleanOnBuildStart("dist"),
+    typescript({
+      tsconfig: "tsconfig.build.json",
+    }),
+  ],
+  external: Object.keys(pkg.dependencies || {}), // https://github.com/rollup/rollup-plugin-node-resolve/issues/77
+};
+
+export default assembleRollupConfig(rollupOptions);
